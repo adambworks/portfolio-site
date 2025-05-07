5e649002bbd69812fb7ea62a636f90d8415884aa
ahb5637 <ahb5637@rit.edu>
2025-05-06 22:11:23 -0400

    working through disel guide

--- portfolio_back/src/main.rs
+++ portfolio_back/src/main.rs
@@ -1,6 +1,38 @@
 pub mod models;
 pub mod schema;
+use diesel::prelude::*;
+use dotenvy::dotenv;
+use std::env;
+
+
+
+
+
+
+pub fn establish_connection() -> PgConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
 
 fn main() {
-    println!("Hello, world!");
+    use self::schema::projects::dsl::*;
+
+    let connection = &mut establish_connection();
+    let results = projects
+        .filter(published.eq(true))
+        .limit(5)
+        .select(Project::as_select())
+        .load(connection)
+        .expect("Error loading posts");
+
+    println!("Displaying {} projects", results.len());
+    for project in results {
+        println!("{}", post.title);
+        println!("-----------\n");
+        println!("{}", post.body);
+    }
 }
