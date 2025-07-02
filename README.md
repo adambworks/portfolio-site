cda079d0df9f233af279d2699a1b9ce902c2dff5
ahb5637 <ahb5637@rit.edu>
2025-07-01 22:19:21 -0400

    update to readme and homepage about section

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# portfolio-site
+## Quick Overview
+The goal is to create a website for viewing all my projects, I want the pages to be dynamically generated so I just have to put data in a db and the pages will be updated for either adding info to a project or even creating a new project.
 
+## More Info
+- [[todo]]
+- [[Database scratchpad]]
+
+## Software Stack
+- postgres
+- React
+- Vite
+- React Router
+- Rust
+- Actix
+- Tailwind
+- Docker
--- portfolio_front/src/pages/HomePage.tsx
+++ portfolio_front/src/pages/HomePage.tsx
@@ -21,11 +21,11 @@ export default function HomePage() {
 			<section className="mb-8">
 				<h2 className="mb-2 text-2xl font-semibold">About This Site</h2>
 				<p className="text-colore">
-					This is a full-stack portfolio site built using Rust (with Actix Web and Diesel) for
-					the backend and React (with Vite React Router and Tailwind css) for the frontend. It showcases a
-					collection of projects, each with detailed chapters and entries that
-					walk through the development process. This site is both a technical
+					This is a custom built portfolio site built using Rust (with Actix Web and Diesel) for
+					the backend, and React (with Vite React Router and Tailwind css) for the frontend. It's purpose is to showcases a
+					collection of projects, each with there own chapters and entries to explain or document the project. This site is both a technical
 					showcase and a place where I document my work and ideas.
+          The code is public, so theoretically anyone could build this site for themselves and have there own portfolio site.
 				</p>
 			</section>
 
